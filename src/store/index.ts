c7f1b2769d19449014d1485e1e4ff733cad5992d
lisulan <2710855993@qq.com>
2022-07-09 15:38:37 +0800

    初始化

---
+++ src/store/index.ts
@@ -0,0 +1,10 @@
+import { createStore } from 'vuex'
+// Create a new store instance.
+const store = createStore({
+    state: {},
+    getters: {},
+    mutations: {},
+    actions: {},
+    modules: {}
+})
+export default store;
